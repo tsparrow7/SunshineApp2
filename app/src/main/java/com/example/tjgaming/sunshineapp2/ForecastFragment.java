dd1368c1b3551e52d3c09f7e6cabec7e73327f8c
tsparrow7 <tsparrow7@yahoo.com>
2016-06-07 02:01:54 +0800

    Completed adding Detail Activity and is populated by data recieved from the intent.

--- app/src/main/java/com/example/tjgaming/sunshineapp2/ForecastFragment.java
+++ app/src/main/java/com/example/tjgaming/sunshineapp2/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.tjgaming.sunshineapp2;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -15,7 +16,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -83,8 +83,14 @@ public class ForecastFragment extends Fragment {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
                 String forecast = mForecastAdapter.getItem(position);
-                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+                Intent detailIntent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(detailIntent);
+//              Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+
+
             }
         });
         return root;
